25b539d732bfb6a97de2ef7618baed8b0a2f1dbe
nturner2019 <nturner@sch.org>
2016-03-11 21:05:46 +0000

    nitrous-backup at Fri Mar 11 21:05:46 UTC 2016

--- js/card.js
+++ js/card.js
@@ -1,8 +1,13 @@
-$( function(){
+$( function(toggle){
 
   $( "#card-one").click(function(){
     //Use toggleClass method here
   });
+  
+  $( ".back panel").click(function(){
+    //Use toggleClass method here
+  });
+
 
 
 	// code click event for card-two here
